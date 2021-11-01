68f95f231dbf6e4fcafe3660376098c1b75be9fd
Nickolay Trukhin <coolone.official@gmail.com>
2021-11-01 18:12:01 +0300

    Initial Commit

---
+++ vktest/vktestApp.swift
@@ -0,0 +1,17 @@
+//
+//  vktestApp.swift
+//  vktest
+//
+//  Created by Nickolay Truhin on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct vktestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
